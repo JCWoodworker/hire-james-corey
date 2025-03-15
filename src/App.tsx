3f2321da11e97782e79853faab86e7829f82cf7a
JCWoodworker <jc@rilocalwoodworks.com>
2025-03-15 10:09:17 -0400

    added social icons and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { Box } from "@mui/material"
 import "./App.css"
+import { LinkedIn, GitHub } from "@mui/icons-material"
 
 function App() {
 	return (
-		<>
+		<Box>
 			<h1>Hire James Corey</h1>
-			<h3>Software Engineer</h3>
-      <p>Portfolio coming soon...</p>
-		</>
+			<h2>Software Engineer</h2>
+			<p>Portfolio coming soon...</p>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+					alignItems: "center",
+					gap: 2,
+				}}
+			>
+				<a href="https://www.linkedin.com/in/j-corey">
+					<LinkedIn sx={{ fontSize: 40 }} />
+				</a>
+				<a href="https://github.com/jcwoodworker">
+					<GitHub sx={{ fontSize: 40 }} />
+				</a>
+			</Box>
+		</Box>
 	)
 }
 
